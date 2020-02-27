2a90b8148c8a9976bcdb54d2505fcdfb59f07bdf
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-02-27 05:58:05 -0600

    server init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+
+
+
+//=============Server Init=======================//
+const server = express();
+const PORT = process.env.PORT || 5555;
+server.listen(PORT, () => {
+  console.log(`//================Server is running on ${PORT}... =================//`);
+});
+//============Server Routers and Middleware=========//
+server.use(cors())
+server.use(express.json())
+server.use(helmet());
