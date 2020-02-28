fb485916da57e73705bef0bf013035d1182cea64
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-02-27 19:18:33 -0600

    added a bunch more models, some new endpoints and more documentation

--- routers/exoplanet-router/exoplanet-router.js
+++ routers/exoplanet-router/exoplanet-router.js
@@ -35,7 +35,33 @@ router.get("/planet/:id",(req,res) => {
 
 
 })
+/**
+ * @api {get} api/exoplanets/facilities
+ * @apiGroup Get Facilities
+ * @apiDescription Get all of the facilites that found each planet
+ */
+router.get("/facilities",(req,res)=> {
+    Model.findAllFacilities()
+    .then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        res.status(500).json({error_message:err})
+    })
+})
 
+/**
+ * @api {get} api/exoplanets/orbitalrange
+ * @apiGroup Get orbital periods
+ * @apiDescription get Planet Orbital Value with the higher and lower uncertainty. Includes Planet name
+ */
+router.get("/orbitalrange",(req,res) => {
+    Model.findOrbitalPeriodValueRange()
+    .then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        res.status(500).json({error_message:err})
+    })
+})
 /**
  * 
  * @api {get} api/exoplanets/search
@@ -56,7 +82,7 @@ router.get("/search",(req,res) => {
  * @api {get} api/exoplanets/query?q=Kepler-153
  * @apiGroup Search
  * @apiParam ${?q=pl_hostname}
- * @apiDescription search the database by name
+ * @apiDescription search the database by hostname
  */
 router.get("/query", (req,res) => {
     let db_query = req.query.q
@@ -69,11 +95,52 @@ router.get("/query", (req,res) => {
     })
 })
 
+/**
+ * @api {get} api/exoplanets/maxaxisvaluerange
+ * @apiGroup Planet Axis Range
+ * @apiDescription Planet Orbital Semi-Major Axis Value [AU]
+ */
+router.get("/maxaxisvaluerange",(req,res) => {
+    Model.findOrbitalMaxAxisValueRange()
+    .then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        res.status(500).json({error_message:err})
+    })
+})
+/**
+ * @api {get} api/exoplanets/maxaxisvaluerange/${id}
+ * @apiParam ${id} ID of the planet you are trying to get axis data on
+ * @apiGroup One Planets Axis Range
+ * @apiDescription Planet Orbital Semi-Major Axis Value [AU]
+ */
+router.get("/maxaxisvaluerange/:id",(req,res) => {
+    let {id} = req.params;
+    Model.findOrbitalMaxAxisValueRangeById(id)
+    .then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        res.status(500).json({error_message:err})
+    })
+})
+// router.get("/pname", (req,res) => {
+//     console.log("HIT THE ROUTE")
+//     let db_query = req.query.q
+//     if(!db_query){
+//         res.status(404).json({error_message:"please type a valid query"})
+//     }
+//     Model.findPlanetByPLName(db_query)
+//     .then(data => {
+//         res.status(200).json(data)
+//     }).catch(err => {
+//         res.status(500).json({error_message:err})
+//     })
+// })
 
 /**
  * @api {get} api/exoplanets/
  * @apiGroup Exoplanets
- * @apiDescription gets a list of planet names
+ * @apiDescription gets a list of planet hostnames
  */
 router.get("/",(req,res) => {
     Model.findAllPlanetNames()
